c5138a7c6f1d4529f9ad0f777daa4f5a2cd9789b
Gabriel Cavallo <cavallo.v.gabriel@gmail.com>
2023-04-19 00:06:54 -0300

    add dockerfile

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  console.log(configService.get('TELEGRAM_API_TOKEN'));
   await app.listen(3000);
 }
 bootstrap();
